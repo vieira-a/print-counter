4d7618d8c60e86f66d0e2c053f2f7628c1696e88
Anderson Vieira <asvieira.br@gmail.com>
2023-04-27 00:27:53 -0300

    feat: add Notification component

---
+++ src/components/Notification/index.tsx
@@ -0,0 +1,56 @@
+import {
+  CheckmarkFilled,
+  WarningFilled,
+  ErrorFilled,
+  Close,
+} from "@carbon/icons-react";
+
+interface NotificationProps {
+  message?: string;
+  theme?: "success" | "warning" | "error";
+  onClick?: () => void;
+  setShowNotification: (state: boolean) => void;
+}
+
+export default function Notification({
+  message,
+  theme,
+  onClick,
+}: NotificationProps) {
+  return (
+    <div
+      className={`p-4 flex justify-between w-[33%] mx-auto absolute top-[192px] left-[50%] translate-x-[-50%]
+      ${
+        theme === "success" &&
+        "bg-notification-success-bg border border-notification-success border-l-[3px] border-l-notification-success"
+      }
+      ${
+        theme === "warning" &&
+        "bg-notification-warning-bg border border-notification-warning border-l-[3px] border-l-notification-warning"
+      }
+      ${
+        theme === "error" &&
+        "bg-notification-error-bg border border-notification-error border-l-[3px] border-l-notification-error"
+      }
+      `}
+    >
+      <div className="flex gap-4 ">
+        <div>
+          {theme === "success" && (
+            <CheckmarkFilled className="text-notification-success" />
+          )}
+          {theme === "warning" && (
+            <WarningFilled className="text-notification-warning" />
+          )}
+          {theme === "error" && (
+            <ErrorFilled className="text-notification-error" />
+          )}
+        </div>
+        <div className="text-xs">{message}</div>
+      </div>
+      <button onClick={onClick}>
+        <Close />
+      </button>
+    </div>
+  );
+}
--- src/features/printer/PrinterForm.tsx
+++ src/features/printer/PrinterForm.tsx
@@ -1,6 +1,8 @@
 import { Close } from "@carbon/icons-react";
 import Input from "../../components/Input";
 import ButtonContent from "../../components/ButtonContent";
+import Notification from "../../components/Notification";
+import { useState } from "react";
 
 interface PrinterFormProps {
   setShowForm: (state: boolean) => void;
@@ -9,11 +11,14 @@ interface PrinterFormProps {
 export default function PrinterForm({ setShowForm }: PrinterFormProps) {
   function closeForm() {
     setShowForm(false);
+    setShowNotification(false);
   }
 
+  const [showNotification, setShowNotification] = useState(false);
+
   return (
-    <section className="w-[50%] px-4 mx-auto bg-carbon-bg-modal absolute top-[192px] left-[50%] translate-x-[-50%] ">
-      <div className="flex justify-between py-4">
+    <section className="w-[50%] px-4 mx-auto bg-carbon-bg-modal absolute top-[192px] left-[50%] translate-x-[-50%]">
+      <div className="flex justify-between py-4 relative">
         <h2>Cadastro de impressoras</h2>
         <Close size={24} onClick={closeForm} className="cursor-pointer" />
       </div>
@@ -62,17 +67,30 @@ export default function PrinterForm({ setShowForm }: PrinterFormProps) {
               placeholder="Informe o contador atual"
             />
           </div>
-          <div className="grid-flow-row text-right">
+          <div className="grid-flow-row text-right px-0">
             <ButtonContent
               onClick={() => setShowForm(false)}
               text="Cancelar"
               theme="secondary"
               className="w-[33%]"
             />
-            <ButtonContent text="Salvar" theme="primary" className="w-[33%]" />
+            <ButtonContent
+              onClick={() => setShowNotification(true)}
+              text="Salvar"
+              theme="primary"
+              className="w-[33%]"
+            />
           </div>
         </form>
       </div>
+      {showNotification && (
+        <Notification
+          setShowNotification={setShowNotification}
+          onClick={() => setShowNotification(false)}
+          theme="success"
+          message="Sucesso"
+        />
+      )}
     </section>
   );
 }
