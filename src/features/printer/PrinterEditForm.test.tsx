d9215985646915d29102535b88777601748d2862
Anderson Vieira <asvieira.br@gmail.com>
2023-05-05 13:45:48 -0300

    feat: add form components

--- src/features/printer/PrinterEditForm.test.tsx
+++ src/features/printer/PrinterEditForm.test.tsx
@@ -1,6 +1,68 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import PrinterFormEdit from "./PrinterFormEdit";
 
-it("Should render PrinterFormEdit component", () => {
-  render(<PrinterFormEdit />);
+describe("Should render PrinterFormEdit components", () => {
+  beforeEach(() => {
+    render(<PrinterFormEdit />);
+  });
+
+  it("Should render form title", () => {
+    const formTitle = screen.getByText("Alteração de impressora");
+    expect(formTitle).toBeInTheDocument();
+  });
+  it("Should render form subtitle", () => {
+    const formSubtitle = screen.getByText(
+      "Altere os campos com as informações necessárias para editar uma impressora cadastrada"
+    );
+    expect(formSubtitle).toBeInTheDocument();
+  });
+  it("Should render serial number label and input type", () => {
+    const labelSerial = screen.getByLabelText("Número de série");
+    const inputSerial = screen.getByPlaceholderText(
+      "Informe o número de série"
+    );
+    expect(labelSerial).toBeInTheDocument();
+    expect(inputSerial).toHaveAttribute("type", "text");
+  });
+  it("Should render brand label and input type", () => {
+    const labelBrand = screen.getByLabelText("Fabricante");
+    const inputBrand = screen.getByPlaceholderText("Informe o fabricante");
+    expect(labelBrand).toBeInTheDocument();
+    expect(inputBrand).toHaveAttribute("type", "text");
+  });
+  it("Should render model label and input type", () => {
+    const labelModel = screen.getByLabelText("Modelo");
+    const inputModel = screen.getByPlaceholderText("Informe o modelo");
+    expect(labelModel).toBeInTheDocument();
+    expect(inputModel).toHaveAttribute("type", "text");
+  });
+  it("Should render localization label and input type", () => {
+    const labelLocal = screen.getByLabelText("Localização");
+    const inputLocal = screen.getByPlaceholderText(
+      "Informe o local de instalação"
+    );
+    expect(labelLocal).toBeInTheDocument();
+    expect(inputLocal).toHaveAttribute("type", "text");
+  });
+  it("Should render counter label and input type", () => {
+    const labelCounter = screen.getByLabelText("Contador atual");
+    const inputCounter = screen.getByPlaceholderText(
+      "Informe o contador atual"
+    );
+    expect(labelCounter).toBeInTheDocument();
+    expect(inputCounter).toHaveAttribute("type", "number");
+  });
 });
+// it("Should render cancel button", () => {
+//   const buttons = screen.getAllByRole("button");
+//   const buttonCancel = buttons.map(
+//     (button) => (button.textContent = "Cancelar")
+//   );
+
+//   expect(buttonCancel).toBeInTheDocument();
+// });
+// it("Should render save button", () => {
+//   const buttons = screen.getAllByRole("button");
+//   const buttonSave = buttons.map((button) => (button.textContent = "Salvar"));
+//   expect(buttonSave).toBeInTheDocument();
+// });
--- src/features/printer/PrinterFormEdit.tsx
+++ src/features/printer/PrinterFormEdit.tsx
@@ -1,7 +1,41 @@
 export default function PrinterFormEdit() {
   return (
     <section>
-      <h1>Editar impressora</h1>
+      <div>
+        <h2>Alteração de impressora</h2>
+      </div>
+      <div>
+        <p>
+          Altere os campos com as informações necessárias para editar uma
+          impressora cadastrada
+        </p>
+      </div>
+      <form>
+        <label>
+          Número de série
+          <input type="text" placeholder="Informe o número de série" />
+        </label>
+
+        <label>
+          Fabricante
+          <input type="text" placeholder="Informe o fabricante" />
+        </label>
+
+        <label>
+          Modelo
+          <input type="text" placeholder="Informe o modelo" />
+        </label>
+
+        <label>
+          Localização
+          <input type="text" placeholder="Informe o local de instalação" />
+        </label>
+
+        <label>
+          Contador atual
+          <input type="number" placeholder="Informe o contador atual" />
+        </label>
+      </form>
     </section>
   );
 }
